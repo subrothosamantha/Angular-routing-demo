238385f9784884ea337cec03c04065165bb4ef56
subrotho samantha <subrotho.samantha@dunnsolutions.com>
2022-03-07 12:49:30 +0530

    latest commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+     path:'about',
+     component:AboutComponent
+  },
+  {
+     path:'login',
+     component:LoginComponent
+  },
+  {
+     path:'',
+     component:HomeComponent
+  },
+  {
+    path:'user/:id',
+    component:UserComponent
+ },
+ {
+   path:'**',
+   component:ErrorpageComponent
+ }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
